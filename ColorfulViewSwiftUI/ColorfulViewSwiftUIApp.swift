06694e757e6de2b7d33d2b988438246a0f70901a
Alexey Mefodyev <al.mefodyev@yandex.ru>
2021-08-07 14:35:16 +0300

    Initial Commit

---
+++ ColorfulViewSwiftUI/ColorfulViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorfulViewSwiftUIApp.swift
+//  ColorfulViewSwiftUI
+//
+//  Created by Alexey Mefodyev on 07.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorfulViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
